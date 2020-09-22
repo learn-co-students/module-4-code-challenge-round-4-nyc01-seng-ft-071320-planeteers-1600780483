7844e75f51a2964f9d67177ded998451e5513e8e
junko911 <junkotahara911@gmail.com>
2020-09-22 09:49:43 -0400

    Toggle bio/quote

--- src/Components/Planeteer.jsx
+++ src/Components/Planeteer.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 class Planeteer extends React.Component {
 
+  state = {
+    clicked: false
+  }
+
+  clickHandler = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
+
   render() {
     return (
       <li className="cards__item">
         <div className="card">
-          <img src={this.props.planeteer.pictureUrl} alt={this.props.planeteer.name} className="card__image" />
+          <img src={this.props.planeteer.pictureUrl} alt={this.props.planeteer.name} className="card__image" onClick={this.clickHandler}/>
           <div className="card__content">
             <div className="card__title">{this.props.planeteer.name}</div>
-            <p className="card__text">{this.props.planeteer.bio}</p>
+            <p className="card__text">{this.state.clicked ? this.props.planeteer.quote : this.props.planeteer.bio}</p>
             <div className="card__detail">
               <p>{this.props.planeteer.twitter}</p>
               <p>{this.props.planeteer.fromUSA ? "USA-based" : "Working overseas"}</p>
