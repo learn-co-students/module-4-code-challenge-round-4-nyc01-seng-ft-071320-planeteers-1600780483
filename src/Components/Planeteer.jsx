e3fcfbfb506fd425e774e826af70f531d24da33f
DBenson1017 <devinbenson@gmail.com>
2020-09-22 10:54:56 -0400

    age deliverable added

--- src/Components/Planeteer.jsx
+++ src/Components/Planeteer.jsx
@@ -13,13 +13,21 @@ class Planeteer extends React.Component {
     }))
   }
 
+  generateAge =()=>{
+    let today = new Date()
+    let year = today.getFullYear()
+    let age = year - this.props.planeteer.born
+   return age 
+
+  }
+
   render() {
     return (
       <li className="cards__item">
         <div className="card">
           <img onClick={this.clickHandler} src={this.props.planeteer.pictureUrl} alt={this.props.planeteer.name} className="card__image" />
           <div className="card__content">
-            <div className="card__title">{this.props.planeteer.name}</div>
+            <div className="card__title">{this.props.planeteer.name}: {this.generateAge()}</div>
             <p className="card__text">{this.state.clicked ? this.props.planeteer.quote : this.props.planeteer.bio}</p>
             <div className="card__detail">
               <p>{this.props.planeteer.twitter}</p>
